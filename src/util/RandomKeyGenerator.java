c1a79733238960efd4240db97bdc7a7b5fc2ecc7
JustAnotherJavaProgrammer <JustAnotherJavaProgrammer@users.noreply.github.com>
2021-08-14 18:13:28 +0200

    Added a `RandomKeyGenerator` in preparation for the addition of OTP

---
+++ src/util/RandomKeyGenerator.java
@@ -0,0 +1,38 @@
+package util;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class RandomKeyGenerator {
+
+	private static SecureRandom csprng;
+
+	private static void initCspring() {
+		if (csprng != null)
+			return;
+		try {
+			csprng = SecureRandom.getInstance("NativeRNG");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			csprng = new SecureRandom();
+		}
+	}
+	
+	public static int generateNumKey(int maxVal) {
+		initCspring();
+		return csprng.nextInt(maxVal);
+	}
+
+	public static String generateKey(int length, char[] allowedChars) {
+		initCspring();
+		StringBuilder keyBuilder = new StringBuilder();
+		for (int i = 0; i < length; i++) {
+			keyBuilder.append(allowedChars[csprng.nextInt(allowedChars.length)]);
+		}
+		return keyBuilder.toString();
+	}
+
+	public static String generateKey(int length, AlphabetMode mode) {
+		return generateKey(length, AlphabetMode.getAllowedChars(mode));
+	}
+}
