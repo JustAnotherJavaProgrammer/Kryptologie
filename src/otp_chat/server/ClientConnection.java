76c434be2e8720a977d2d5e9638c5156b497f5f4
JustAnotherJavaProgrammer <JustAnotherJavaProgrammer@users.noreply.github.com>
2021-08-24 22:30:26 +0200

    Implemented the server

--- src/otp_chat/server/ClientConnection.java
+++ src/otp_chat/server/ClientConnection.java
@@ -1,14 +1,69 @@
 package otp_chat.server;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import otp_chat.OTPMessage;
+import otp_chat.ReceivedMessage;
+
 public class ClientConnection implements Runnable {
 
-	public ClientConnection(String nickname) {
-		
+	public final Thread thread;
+	public final String nickname;
+	private final Server server;
+	private final Socket socket;
+
+	public ClientConnection(String nickname, Socket sock, Server server) {
+		this.server = server;
+		this.socket = sock;
+		this.nickname = nickname;
+
+		this.thread = new Thread(this);
+		this.thread.start();
 	}
-	
+
 	@Override
 	public void run() {
-		
+		try {
+			ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+			while (socket.isConnected() && !Thread.interrupted()) {
+				switch (in.read()) {
+				case 0:
+					server.broadcast((OTPMessage) in.readObject());
+					break;
+				case 1: {
+					String[] nicknames = new String[server.connections.size()];
+					for (int i = 0; i < nicknames.length; i++) {
+						nicknames[i] = server.connections.get(i).nickname;
+					}
+					new ObjectOutputStream(socket.getOutputStream()).writeObject(nicknames);
+				}
+					break;
+				default:
+					break;
+				}
+			}
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		server.disconnected(this);
 	}
-	
+
+	public boolean send(OTPMessage message) throws IOException {
+		socket.getOutputStream().write(0);
+		boolean hasPersonalizedKey = message.personalizedKeys.containsKey(nickname);
+		ReceivedMessage mess = hasPersonalizedKey
+				? new ReceivedMessage(message.personalizedKeys.get(nickname), message.ciphertext, message.alphabet)
+				: new ReceivedMessage(message.key, message.ciphertext, message.alphabet);
+		new ObjectOutputStream(socket.getOutputStream()).writeObject(mess);
+		return hasPersonalizedKey;
+	}
+
 }
--- src/otp_chat/server/Server.java
+++ src/otp_chat/server/Server.java
@@ -1,23 +1,58 @@
 package otp_chat.server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import otp_chat.OTPMessage;
 
 public class Server {
 	public final int port;
 	private final ConnectionListener listener;
-	
-	
+	List<ClientConnection> connections = Collections.synchronizedList(new ArrayList<>());
+
 	public Server(int port, ConnectionListener listener) {
 		this.port = port;
 		this.listener = listener;
 	}
-	
+
 	public void start() throws IOException {
 		ServerSocket s = new ServerSocket(port);
-		while(!Thread.interrupted()) {
-			// TODO do something
+		while (!Thread.interrupted()) {
+			Socket conn = s.accept();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String nick = reader.readLine();
+			ClientConnection cCon = new ClientConnection(nick, conn, this);
+			connections.add(cCon);
+			listener.onConnect(cCon);
 		}
+		s.close();
+	}
+
+	public void broadcast(OTPMessage message) {
+		for (ClientConnection conn : connections) {
+			try {
+				conn.send(message);
+			} catch (IOException e) {
+				System.err.println("Broadcasting message to " + conn.nickname + " failed:");
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void disconnected(ClientConnection conn) {
+		connections.remove(conn);
+		listener.onDisconnect(conn);
+	}
+
+	public ClientConnection[] getConnections() {
+		ClientConnection[] conns = new ClientConnection[0];
+		return connections.toArray(conns);
 	}
 
 }
